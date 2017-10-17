a328e5da57f066d3458323bc523c298f6a12871a
gohzenhao <gohzenhao@gmail.com>
2017-10-17 22:10:09 +1000

    Added Driver and Store class.

---
+++ Store.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Store {
+	
+	public String customerType;
+	public double discountFactor;
+	public int numberOfFortnight;
+	public List<Integer> productPrice;
+	public List<Integer> fundAtTheBeginning;
+	public List<double[][]> matrices;
+	
+
+	
+	public Store(){
+		
+	}
+}
