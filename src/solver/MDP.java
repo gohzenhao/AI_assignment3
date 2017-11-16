75c2d7ad137a2d59f7d62f80b3e8f581cdeed06a
gohzenhao <gohzenhao@gmail.com>
2017-11-16 14:34:28 +1000

    Created value iteration. It works but not correctly. Able to choose best action for a given state.

--- src/solver/MDP.java
+++ src/solver/MDP.java
@@ -3,161 +3,329 @@ package solver;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
+import problem.Matrix;
 import problem.ProblemSpec;
 import problem.VentureManager;
 
 public class MDP {
 	
 	
-	private ProblemSpec problemSpec;
-	private VentureManager ventureManager;
-	private HashMap<Integer,Integer> generateState = new HashMap<Integer,Integer>();
-	private List<Integer> currentAction;
-	private HashMap<List<Integer>,List<Integer>> allStates = new HashMap<List<Integer>,List<Integer>>();
-	private HashMap<List<Integer>,List<Integer>> allActions = new HashMap<List<Integer>,List<Integer>>();
-	private HashMap<List<Integer>,List<List<Integer>>> actionsForStates = new HashMap<List<Integer>,List<List<Integer>>>();
-
+	public ProblemSpec problemSpec;
+	public HashMap<Integer,Integer> generateState = new HashMap<Integer,Integer>();
+	public List<Integer> currentAction;
+	public HashMap<Integer,List<Integer>> allStates = new HashMap<Integer,List<Integer>>();
+	public HashMap<List<Integer>,List<List<Integer>>> actionsForAState = new HashMap<List<Integer>,List<List<Integer>>>();
+	public List<Integer> currentState;
 	
-	public MDP(ProblemSpec inProblem){		
-		problemSpec = inProblem;
-//		ventureManager = problemSpec.getVentureManager();
-		ventureManager = new VentureManager("bronze");
+	public HashMap<List<Integer>,List<Double>> rewardForAState = new HashMap<List<Integer>,List<Double>>();
+	
+	public List<Matrix> newProb = new ArrayList<Matrix>();
+	
+	public int hashMapCounter = 0;
+	
+	
+	public MDP(ProblemSpec inProblem){
+		
+		this.problemSpec = inProblem;
+		
 	}
 	
 	public void generateStates(){
-		generateActions();
-		System.out.println("Problem loaded : "+this.problemSpec.isModelLoaded());		
-		System.out.println("Generating states... ");		
+		
+		VentureManager ventureManager = this.problemSpec.getVentureManager();
+		
+		System.out.println("Problem loaded : "+this.problemSpec.isModelLoaded());
+		
+		System.out.println("Generating states... ");
+		
 		int maxFunds = ventureManager.getMaxManufacturingFunds();
+		
+		
+		List<Integer> fundsForFirstVenture = new ArrayList<Integer>();
 		List<Integer> validState = new ArrayList<Integer>();
+		for(int i=0;i<maxFunds+1;i++){
+			fundsForFirstVenture.add(i);
+			
+		}
+		
 		//First Venture
 		for(int i=0;i<maxFunds+1;i++){
 			//Second Venture
-			for(int u=0;u<maxFunds+1;u++){				
-				if(ventureManager.getNumVentures()==2){					
-					if((i+u)<=maxFunds){
-						validState.add(i);
+			for(int u=0;u<maxFunds+1;u++){
+				
+				if(this.problemSpec.getVentureManager().getNumVentures()==2){
+					
+					if((fundsForFirstVenture.get(i)+u)<=maxFunds){
+						validState.add(fundsForFirstVenture.get(i));
 						validState.add(u);
-						allStates.put(validState, validState);
-						generateActionsForState(validState);
-						validState = new ArrayList<Integer>();							
+						allStates.put(hashMapCounter, validState);
+						validState = new ArrayList<Integer>();
+						hashMapCounter++;
+						
 					}
 				}
 				else{
 					//Third Venture
 					for(int e=0;e<maxFunds+1;e++){
-						if((i+u+e)<=maxFunds){
-							validState.add(i);
+						if((fundsForFirstVenture.get(i)+u+e)<=maxFunds){
+							validState.add(fundsForFirstVenture.get(i));
 							validState.add(u);
 							validState.add(e);
-							allStates.put(validState, validState);
-							generateActionsForState(validState);
+							allStates.put(hashMapCounter, validState);
 							validState = new ArrayList<Integer>();
+							hashMapCounter++;
 						}
-					}
-				}				
-			}
-		}
-		
-	}
-	
-	/**
-	 * generate all possible actions for different venture manager
-	 */
-	public void generateActions()
-	{
-		Integer maxAdditionalFunding = ventureManager.getMaxAdditionalFunding();
-		List<Integer> validAction = new ArrayList<Integer>();
-		for(int i=0;i<maxAdditionalFunding+1;i++){
-			//Second Venture
-			for(int u=0;u<maxAdditionalFunding+1;u++){				
-				if(ventureManager.getNumVentures()==2){					
-					if((i+u)<=maxAdditionalFunding){
-						validAction.add(i);
-						validAction.add(u);
-						allActions.put(validAction, validAction);
-						validAction = new ArrayList<Integer>();					
 					}
 				}
-				else{
-					//Third Venture
-					for(int e=0;e<maxAdditionalFunding+1;e++){
-						if((i+u+e)<=maxAdditionalFunding){
-							validAction.add(i);
-							validAction.add(u);
-							validAction.add(e);
-							allActions.put(validAction, validAction);
-							validAction = new ArrayList<Integer>();
-						}
-					}
-				}				
+				
 			}
+
+		}
+		
+		
+	}
+	
+	public void generateActionsForEachState(){
+		
+		VentureManager ventureManager = this.problemSpec.getVentureManager();
+		
+		int maxAdditionalFund = ventureManager.getMaxAdditionalFunding();
+		
+		int maxFund = ventureManager.getMaxManufacturingFunds();
+		
+		int size = this.allStates.size();
+		
+		List<List<Integer>> validAction = new ArrayList<List<Integer>>();
+		
+		for(int i=0;i<size;i++){
+			
+			this.currentState = this.allStates.get(i);
+			
+			for(int u=0;u<size;u++){
+				
+				List<Integer> pendingState = this.allStates.get(u);
+				
+				if(ventureManager.getName().equals("bronze")){
+						
+					int currentFund = this.currentState.get(0)+this.currentState.get(1);
+					int allocatedFund = pendingState.get(0)+pendingState.get(1);
+					boolean validation1 = (this.currentState.get(0) + pendingState.get(0))<maxFund+1;
+					boolean validation2 = (pendingState.get(1) + this.currentState.get(1))<maxFund+1;
+					boolean validation3 = (pendingState.get(0) + pendingState.get(1))<maxAdditionalFund+1;
+					boolean validation4 = (currentFund+allocatedFund)<maxFund+1;
+					if(validation1 && validation2 && validation3 && validation4){
+						validAction.add(pendingState);
+					}
+				}
+				
+				else if(ventureManager.getName().equals("silver")){
+					
+					int currentFund = this.currentState.get(0)+this.currentState.get(1);
+					int allocatedFund = pendingState.get(0)+pendingState.get(1);
+					boolean validation1 = (this.currentState.get(0) + pendingState.get(0))<maxFund+1;
+					boolean validation2 = (pendingState.get(1) + this.currentState.get(1))<maxFund+1;
+					boolean validation3 = (pendingState.get(0) + pendingState.get(1))<maxAdditionalFund+1;
+					boolean validation4 = (currentFund+allocatedFund)<maxFund+1;
+					if(validation1 && validation2 && validation3 && validation4){
+						validAction.add(pendingState);
+					}
+				}
+				
+				else if(ventureManager.getName().equals("gold")){
+					
+					int currentFund = this.currentState.get(0)+this.currentState.get(1)+this.currentState.get(2);
+					int allocatedFund = pendingState.get(0)+pendingState.get(1)+pendingState.get(2);
+					boolean validation1 = (this.currentState.get(0) + pendingState.get(0))<maxFund+1;
+					boolean validation2 = (pendingState.get(1) + this.currentState.get(1))<maxFund+1;
+					boolean validation3 = (this.currentState.get(2) + pendingState.get(2))<maxFund+1;
+					boolean validation4 = (pendingState.get(0) + pendingState.get(1) + pendingState.get(2))<maxAdditionalFund+1;
+					boolean validation5 = (currentFund+allocatedFund)<maxFund+1;
+					if(validation1 && validation2 && validation3 && validation4 && validation5){
+						validAction.add(pendingState);
+					}
+				}
+				
+				else if(ventureManager.getName().equals("platinum")){
+					
+					int currentFund = this.currentState.get(0)+this.currentState.get(1)+this.currentState.get(2);
+					int allocatedFund = pendingState.get(0)+pendingState.get(1)+pendingState.get(2);
+					boolean validation1 = (this.currentState.get(0) + pendingState.get(0))<maxFund+1;
+					boolean validation2 = (pendingState.get(1) + this.currentState.get(1))<maxFund+1;
+					boolean validation3 = (this.currentState.get(2) + pendingState.get(2))<maxFund+1;
+					boolean validation4 = (pendingState.get(0) + pendingState.get(1) + pendingState.get(2))<maxAdditionalFund+1;
+					boolean validation5 = (currentFund+allocatedFund)<maxFund+1;
+					if(validation1 && validation2 && validation3 && validation4 && validation5){
+						validAction.add(pendingState);
+					}
+				}
+			}
+			
+			this.actionsForAState.put(currentState,validAction);
+			validAction = new ArrayList<List<Integer>>();
+		}
+		
+		
+		
+		
+		
+		
+
+	}
+	
+	public void generateMatrix(){
+		
+		for(int i=0;i<this.problemSpec.getVentureManager().getNumVentures();i++){
+			Matrix currentMatrix = this.problemSpec.getProbabilities().get(i);
+			int maxFunds = this.problemSpec.getVentureManager().getMaxManufacturingFunds();
+			double[][] matrixData = new double[maxFunds+1][maxFunds+1];
+			
+			for(int r=0;r<maxFunds+1;r++){
+				
+				
+				for(int c=0;c<maxFunds+1;c++){
+					
+
+					if(c>r){
+						matrixData[r][c] = 0.0;
+					}
+					else if(c>0 && c<=r){
+						matrixData[r][c] = currentMatrix.get(r,r-c);
+					}
+					else if(c==0){
+						double totalProb = 0;
+						for(int u=r;u<maxFunds+1;u++){
+							totalProb += currentMatrix.get(r,u);
+						}
+						matrixData[r][c] = totalProb;
+					}
+					
+				}
+				
+				
+			}
+
+			this.newProb.add(new Matrix(matrixData));
+			System.out.println("DONE!");
+			
 		}
 	}
 	
-	public void generateActionsForState(List<Integer> validState){
-		int v1,v2,v3=0;
-		List<List<Integer>> allPossibleActions = new ArrayList<List<Integer>>(); 
-		v1 = validState.get(0);
-		v2 = validState.get(1);
-		String name = ventureManager.getName();
-		if(name.equals("gold") || name.equals("platinum"))
-			v3 = validState.get(2);
-		//loop through allActions
-		Iterator it = allActions.entrySet().iterator();
-	    while (it.hasNext()) {
-	        HashMap.Entry pair = (HashMap.Entry)it.next();
-	        List<Integer> action = (ArrayList<Integer>) pair.getValue();
-	        int a1,a2,a3,totalFund;
-	        a1 = action.get(0);
-	        a2 = action.get(1);
-	        totalFund = a1+a2+v1+v2;
-			if(name.equals("gold") || name.equals("platinum"))
-			{
-				a3 = action.get(2);
-				totalFund = a1+a2+a3+v1+v2+v3;
+//	public void generateRewards(){
+//		
+//		double profit = 0;
+//		double loss = 0;
+//		double immediateReward = 0;
+////		List<Double> immediateRewards = new ArrayList<Double>();
+//		for(int k=0;k<this.allStates.size();k++){
+//			List<Integer> currentState = this.allStates.get(k); 
+//			immediateReward = 0;
+////			immediateRewards = new ArrayList<Double>();
+//			for(int u=0;u<this.problemSpec.getVentureManager().getNumVentures();u++){
+//				
+//				int venture1InitialFunds = currentState.get(u);
+//			    for(int i=1;i<this.problemSpec.getVentureManager().getMaxManufacturingFunds()+1;i++){
+//			    	
+//			    	profit += Math.min(i, venture1InitialFunds) * this.problemSpec.getProbabilities().get(u).get(venture1InitialFunds, i)
+//			    			*this.problemSpec.getSalePrices().get(u);
+//			    	
+//			    }
+//			    
+//			    for(int i=venture1InitialFunds+1;i<this.problemSpec.getVentureManager().getMaxManufacturingFunds()+1;i++){
+//			    	loss += (i-venture1InitialFunds)*this.problemSpec.getProbabilities().get(u).get(venture1InitialFunds, i);
+//
+//
+//			    }
+//			    profit = profit*0.6;
+//			    loss = loss*0.25;
+//			    immediateReward +=  (profit - loss);
+////			    immediateRewards.add(immediateReward);
+//			    
+//			    profit = 0;
+//			    loss = 0;
+//			}
+//			 this.rewardForAState.put(currentState,immediateReward);
+//		}
+		
+	public void generateRewards(){
+		
+		double profit = 0;
+		double loss = 0;
+		double immediateReward = 0;
+		List<Double> immediateRewards = new ArrayList<Double>();
+		for(int k=0;k<this.allStates.size();k++){
+			List<Integer> currentState = this.allStates.get(k); 
+			immediateReward = 0;
+			immediateRewards = new ArrayList<Double>();
+			for(int u=0;u<this.problemSpec.getVentureManager().getNumVentures();u++){
+				
+				int venture1InitialFunds = currentState.get(u);
+			    for(int i=1;i<this.problemSpec.getVentureManager().getMaxManufacturingFunds()+1;i++){
+			    	
+			    	profit += Math.min(i, venture1InitialFunds) * this.problemSpec.getProbabilities().get(u).get(venture1InitialFunds, i)
+			    			*this.problemSpec.getSalePrices().get(u);
+			    	
+			    }
+			    
+			    for(int i=venture1InitialFunds+1;i<this.problemSpec.getVentureManager().getMaxManufacturingFunds()+1;i++){
+			    	loss += (i-venture1InitialFunds)*this.problemSpec.getProbabilities().get(u).get(venture1InitialFunds, i);
+
+
+			    }
+			    profit = profit*0.6;
+			    loss = loss*0.25;
+			    immediateReward =  (profit - loss);
+			    immediateRewards.add(immediateReward);
+			    
+			    profit = 0;
+			    loss = 0;
 			}
-			if(totalFund <= ventureManager.getMaxAdditionalFunding())
-				allPossibleActions.add(action);
-	    }
-	    actionsForStates.put(validState, allPossibleActions);
+			 this.rewardForAState.put(currentState,immediateRewards);
+		}
+
+	    
 	}
 	
-	public static void main(String[] args) throws IOException{
-		
-		String filePath = "C:\\Users\\User-PC\\eclipse-workspace\\AI-ass3\\testcases\\bronze1.txt";
-		
-		ProblemSpec problem = new ProblemSpec(filePath);
-		
-		MDP mdp = new MDP(problem);
-		
-		mdp.generateStates();
-		mdp.generateActions();
-		System.out.println(mdp.actionsForStates.size());
-		System.out.println("All possible states : ");
-		Iterator it = mdp.allActions.entrySet().iterator();
-	    while (it.hasNext()) {
-	        HashMap.Entry pair = (HashMap.Entry)it.next();
-	        List<Integer> state = (ArrayList<Integer>) pair.getValue();
-	        System.out.println(state);
-	        System.out.println("Possible actions:");	        
-	        List<List<Integer>> actions= mdp.actionsForStates.get(state);
-	        System.out.println(actions.size());
-		    for(int i=0;i<actions.size();i++)
-		    {
-		    	System.out.println("Action "+(i+1)+":"+actions.get(i));
-		    }		        
-	    }
-    
-		
-//		System.out.println("All possible actions : ");
-//		System.out.println(mdp.allActions.toString());
-//		System.out.println(mdp.allStates.size());
-//		System.out.println(mdp.allActions.size());
-		
-	}
+//	public static void main(String[] args) throws IOException{
+//		
+//		String filePath = "C:\\Users\\gohzenhao\\workspace\\Assignment3\\bronze1.txt";
+//		
+//		ProblemSpec problem = new ProblemSpec(filePath);
+//		
+//		MDP mdp = new MDP(problem);
+//		
+//		mdp.generateStates();
+//		
+////		System.out.println("All possible states : ");
+////		
+////		for(int i=0;i<mdp.hashMapCounter;i++){
+////			System.out.println(mdp.allStates.get(i));
+////		}
+//		
+//		System.out.println("Size of possible states : "+mdp.allStates.size());
+//		
+//		mdp.generateActionsForEachState();
+//		
+//		for(int i=0;i<mdp.hashMapCounter;i++){
+//			
+//			System.out.println("Possible actions for state : "+mdp.allStates.get(i));
+//			List<List<Integer>> validAction = mdp.actionsForAState.get(mdp.allStates.get(i));
+//			for(int u=0;u<validAction.size();u++){
+//				System.out.println(validAction.get(u));
+//			}
+//		}
+//		
+//		for(int i=0;i<problem.getVentureManager().getMaxManufacturingFunds();i++){
+//			
+//			
+//			for(int u=0;u<problem.getVentureManager().getMaxManufacturingFunds();u++){
+//				
+//				System.out.println(problem.getProbabilities().get(0).get(i, u));
+//			}
+//		}
+//		
+//		
+//	}
 
 }
--- src/solver/MySolver.java
+++ src/solver/MySolver.java
@@ -8,6 +8,7 @@ package solver;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import problem.VentureManager;
@@ -18,18 +19,170 @@ public class MySolver implements FundingAllocationAgent {
 	
 	private ProblemSpec spec = new ProblemSpec();
 	private VentureManager ventureManager;
-    private List<Matrix> probabilities;    
-    private MDP mdp;
+    private List<Matrix> probabilities;
+    public HashMap<List<Integer>,List<Integer>> bestAction = new HashMap<List<Integer>,List<Integer>>();
+    public MDP mdp;
 	
 	public MySolver(ProblemSpec spec) throws IOException {
 	    this.spec = spec;
 		ventureManager = spec.getVentureManager();
         probabilities = spec.getProbabilities();
-        mdp =  new MDP(spec);
+        this.mdp =  new MDP(spec);
 	}
 	
 	public void doOfflineComputation() {
-	    // TODO Write your own code here.
+		
+		HashMap<List<Integer>,Double> valueForStates = new HashMap<List<Integer>,Double>();
+		double discountFactor = this.spec.getDiscountFactor();
+		
+	    mdp.generateStates();
+	    mdp.generateActionsForEachState();
+	    for(int i=0;i<mdp.allStates.size();i++){
+	    	valueForStates.put(mdp.allStates.get(i), 0.0);
+	    }
+	    
+	    mdp.generateRewards();
+	    mdp.generateMatrix();
+	    
+	    Matrix probV1 = mdp.newProb.get(0);
+	    Matrix probV2 = mdp.newProb.get(1);
+	    for(int k=0;k<3;k++){
+	    	for(int j=0;j<mdp.allStates.size();j++){
+	    		
+	    		if(this.ventureManager.getName().equals("bronze") || this.ventureManager.getName().equals("silver")){
+		    	    List<Integer> currentState = mdp.allStates.get(j);
+		    	    List<List<Integer>> possibleActions = mdp.actionsForAState.get(currentState);
+		    	    double currentRewardV1 = mdp.rewardForAState.get(currentState).get(0);
+		    	    double currentRewardV2 = mdp.rewardForAState.get(currentState).get(1);
+	
+		    	    List<Integer> highestValueAction = new ArrayList<Integer>();
+		    	    double highestValue = 0;
+		    		
+				    for(int i=0;i<possibleActions.size();i++){
+				    	List<Integer> newState = new ArrayList<Integer>();
+				    	List<Integer> currentAction = possibleActions.get(i);
+				    	int v1 = currentAction.get(0) + currentState.get(0);
+				    	int v2 = currentAction.get(1) + currentState.get(1);
+				    	newState.add(v1);
+				    	newState.add(v2);
+				    	double newValueV1 = 0;
+				    	double newValueV2 = 0;
+				    	for(int u=0;u<v1+1;u++){
+				    		
+				    		double prob = probV1.get(v1, u);
+				    		double value = valueForStates.get(newState);
+	//			    		System.out.println("--"+value);
+				    		
+				    		newValueV1 += prob*value;
+				    		
+				    		
+				    	}
+				    	
+				    	for(int u=0;u<v2+1;u++){
+				    		
+				    		double prob = probV2.get(v2, u);
+				    		double value = valueForStates.get(newState);
+				    		
+				    		newValueV2 += prob*value;
+				    		
+				    		
+				    	}
+				    	
+				    	double totalValue = newValueV1*newValueV2;
+				    	if(totalValue>highestValue){
+				    		highestValue = totalValue;
+				    		highestValueAction = currentAction;
+				    	}
+				    	
+				    }
+				    
+				    double valueIteration = (currentRewardV1+currentRewardV2) + discountFactor*highestValue;
+				    valueForStates.put(currentState,valueIteration);
+				    this.bestAction.put(currentState, highestValueAction);
+				    System.out.println("Current reward after value iteration for state : "+currentState+" is : "+valueForStates.get(currentState));
+			    }
+	    		else{
+	    			Matrix probV3 = mdp.newProb.get(2);
+	    			List<Integer> currentState = mdp.allStates.get(j);
+		    	    List<List<Integer>> possibleActions = mdp.actionsForAState.get(currentState);
+		    	    double currentRewardV1 = mdp.rewardForAState.get(currentState).get(0);
+		    	    double currentRewardV2 = mdp.rewardForAState.get(currentState).get(1);
+		    	    double currentRewardV3 = mdp.rewardForAState.get(currentState).get(2);
+	
+		    	    List<Integer> highestValueAction = new ArrayList<Integer>();
+		    	    double highestValue = 0;
+		    		
+				    for(int i=0;i<possibleActions.size();i++){
+				    	List<Integer> newState = new ArrayList<Integer>();
+				    	List<Integer> currentAction = possibleActions.get(i);
+				    	int v1 = currentAction.get(0) + currentState.get(0);
+				    	int v2 = currentAction.get(1) + currentState.get(1);
+				    	int v3 = currentAction.get(2) + currentState.get(2);
+				    	newState.add(v1);
+				    	newState.add(v2);
+				    	newState.add(v3);
+				    	double newValueV1 = 0;
+				    	double newValueV2 = 0;
+				    	double newValueV3 = 0;
+				    	for(int u=0;u<v1+1;u++){
+				    		
+				    		double prob = probV1.get(v1, u);
+				    		double value = valueForStates.get(newState);
+	//			    		System.out.println("--"+value);
+				    		
+				    		newValueV1 += prob*value;
+				    		
+				    		
+				    	}
+				    	
+				    	for(int u=0;u<v2+1;u++){
+				    		
+				    		double prob = probV2.get(v2, u);
+				    		double value = valueForStates.get(newState);
+				    		
+				    		newValueV2 += prob*value;
+				    		
+				    		
+				    	}
+				    	
+				    	for(int u=0;u<v3+1;u++){
+				    		
+				    		double prob = probV3.get(v3, u);
+				    		double value = valueForStates.get(newState);
+				    		
+				    		newValueV3 += prob*value;
+				    		
+				    		
+				    	}
+				    	
+				    	double totalValue = newValueV1*newValueV2*newValueV3;
+				    	if(totalValue>highestValue){
+				    		highestValue = totalValue;
+				    		highestValueAction = currentAction;
+				    	}
+				    	
+				    }
+				    
+				    double valueIteration = (currentRewardV1+currentRewardV2+currentRewardV3) + discountFactor*highestValue;
+				    valueForStates.put(currentState,valueIteration);
+				    this.bestAction.put(currentState, highestValueAction);
+	    			
+	    		}
+	    	}
+	    }
+	    
+//	    double valueIteration = (currentRewardV1+currentRewardV2) + discountFactor*highestValue;
+//	    valueForStates.put(currentState,valueIteration);
+//	    
+//	    System.out.println("Current reward after value iteration for state : "+currentState+" is : "+valueForStates.get(currentState));
+	    
+//	    System.out.println("After running value iteration for a few times");
+//	    for(int i=0;i<mdp.allStates.size();i++){
+//	    	System.out.println("Best action for state : "+mdp.allStates.get(i)+" is : "+bestAction.get(mdp.allStates.get(i)));
+//	    }
+	    
+	    
+	    
 	}
 	
 	public List<Integer> generateAdditionalFundingAmounts(List<Integer> manufacturingFunds,
@@ -37,24 +190,26 @@ public class MySolver implements FundingAllocationAgent {
 		// Example code that allocates an additional $10 000 to each venture.
 		// TODO Replace this with your own code.
 
-		List<Integer> additionalFunding = new ArrayList<Integer>();
-		System.out.println("Hey hey");
-		int totalManufacturingFunds = 0;
-		for (int i : manufacturingFunds) {
-			totalManufacturingFunds += i;
-		}
+//		List<Integer> additionalFunding = new ArrayList<Integer>();
+//		System.out.println("Hey hey");
+//		int totalManufacturingFunds = 0;
+//		for (int i : manufacturingFunds) {
+//			totalManufacturingFunds += i;
+//		}
+//		
+//		int totalAdditional = 0;
+//		for (int i = 0; i < ventureManager.getNumVentures(); i++) {
+//			if (totalManufacturingFunds >= ventureManager.getMaxManufacturingFunds() ||
+//			        totalAdditional >= ventureManager.getMaxAdditionalFunding()) {
+//				additionalFunding.add(0);
+//			} else {
+//				additionalFunding.add(1);
+//				totalAdditional ++;
+//				totalManufacturingFunds ++;
+//			}
+//		}
 		
-		int totalAdditional = 0;
-		for (int i = 0; i < ventureManager.getNumVentures(); i++) {
-			if (totalManufacturingFunds >= ventureManager.getMaxManufacturingFunds() ||
-			        totalAdditional >= ventureManager.getMaxAdditionalFunding()) {
-				additionalFunding.add(0);
-			} else {
-				additionalFunding.add(1);
-				totalAdditional ++;
-				totalManufacturingFunds ++;
-			}
-		}
+		List<Integer> additionalFunding = this.bestAction.get(manufacturingFunds);
 
 		return additionalFunding;
 	}
